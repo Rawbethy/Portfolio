7b3346826a9b132951b562b03b6cdbb741d7d02e
Rawbethy <robert@Purgatory.localdomain>
2023-06-04 23:58:13 -0500

    060423

--- client/src/components/body.tsx
+++ client/src/components/body.tsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import SimpleImageSlider from 'react-simple-image-slider';
 import './styles.css'
 
 export default class Body extends Component {
@@ -7,6 +8,12 @@ export default class Body extends Component {
         const h2Style = {
             fontSize: '30px',
         };
+
+        const sliderImages = [
+            {url: "https://img.freepik.com/free-photo/wide-angle-shot-singletree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"},
+            {url: '../Utils/images/img639.jpg'},
+            {url: '../Utils/images/img640.jpg'}
+        ]
 
         return (
             <div className="body">
@@ -19,9 +26,21 @@ export default class Body extends Component {
                         <h2 style={h2Style}>Please enjoy my personal portfolio!</h2>
                     </div>
                 </section>     
-                <section className='about'>
+                <section className='about' id='section-about'>
                     <div className='sub-title' id='about'>
                         <h2>This is the About component</h2>
+                    </div>
+                    <div className='hobbies' id='hobbies'>
+                        <h3>Photography</h3>
+                        <SimpleImageSlider 
+                            width={document.getElementById('hobbies').offsetWidth}
+                            height={document.getElementById('hobbies').offsetHeight}
+                            images={sliderImages}
+                            autoPlay={true}
+                            autoPlayDelay={2.5}
+                            showNavs={false}
+                            showBullets={false}
+                        />
                     </div>
                 </section>
                 <section className="projects">
