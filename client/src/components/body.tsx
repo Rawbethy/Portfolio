7b35daa13c8ae1fa00371f3376c123cd00f4b292
Rawbethy <robert.duque1998@gmail.com>
2023-06-15 17:31:51 -0500

    061523#1

--- client/src/components/body.tsx
+++ client/src/components/body.tsx
@@ -4,45 +4,64 @@ import Desc from '../Utils/Descriptions';
 import './styles.css';
 
 export default class Body extends Component<any, any> {
+
+  home_ref: React.RefObject<HTMLDivElement>;
+  about_title_ref: React.RefObject<HTMLDivElement>;
+  about_who_ref: React.RefObject<HTMLDivElement>;
+  images_ref: React.RefObject<HTMLDivElement>;
+  projects_ref: React.RefObject<HTMLDivElement>;
+  references: any[] = [];
+  ref1 = {}
+  
+
   constructor(props: {}) {
     super(props);
     this.state = {
       width: Number,
       height: Number,
-      homeVisible: null,
-      aboutVisible: null,
-      imagesVisible: null,
-      projectsVisible: null
+      home_visible: null,
+      about_title_visible: null,
+      about_who_visible: null,
+      images_visible: null,
+      projects_visible: null
     };
 
-    this.homeRef = React.createRef();
-    this.aboutRef = React.createRef();
-    this.imagesRef = React.createRef();
-    this.projectsRef = React.createRef();
+    this.home_ref = React.createRef();
+    this.about_title_ref = React.createRef();
+    this.about_who_ref = React.createRef();
+    this.images_ref = React.createRef();
+    this.projects_ref = React.createRef();
+
+    this.ref1 = {
+      'home-title': [this.home_ref, 'home_visible'],
+      'about-title': [this.about_title_ref, 'about_title_visible'],
+      'about-who': [this.about_who_ref, 'about_who_visible'],
+      'imageSlider': [this.images_ref, 'images_visible'],
+      'project-title': [this.projects_ref, 'projects_visible']
+    }
+
+    this.references.push({key: 'home-title', value: [this.home_ref, 'projects_visible']},
+                          {key: 'about-title', value: [this.about_title_ref, 'projects_visible']},
+                          {key: 'about-who', value: [this.about_who_ref, 'projects_visible']},
+                          {key: 'imageSlider', value: [this.images_ref, 'projects_visible']},
+                          {key: 'project-title', value: [this.projects_ref, 'projects_visible']});
+    
   }
 
   componentDidMount() {
     this.updateDimensions();
     window.addEventListener('resize', this.updateDimensions);
 
-    const observerHome = new IntersectionObserver((entries1) => {
-      const entry1 = entries1[0];
-      this.setState({homeVisible: entry1.isIntersecting})
-    })
-
-    const observerAbout = new IntersectionObserver((entries2) => {
-      const entry2 = entries2[0];
-      this.setState({aboutVisible: entry2.isIntersecting})
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        let currState = this.ref1[entry.target.className][1];
+        this.setState({[currState]: entry.isIntersecting});
+      })
     })
     
-    const observerProjects = new IntersectionObserver((entries3) => {
-      const entry3 = entries3[0];
-      this.setState({projectsVisible: entry3.isIntersecting})
-    })
-
-    observerHome.observe(this.homeRef.current);
-    observerAbout.observe(this.aboutRef.current);
-    observerProjects.observe(this.projectsRef.current);
+    for(const key in this.ref1) {
+      observer.observe(this.ref1[key][0].current);
+    }
   }
 
   componentWillUnmount() {
@@ -53,13 +72,8 @@ export default class Body extends Component<any, any> {
     this.updateDimensions();
   }
 
-  homeRef: React.RefObject<HTMLDivElement>;
-  aboutRef: React.RefObject<HTMLDivElement>;
-  imagesRef: React.RefObject<HTMLDivElement>;
-  projectsRef: React.RefObject<HTMLDivElement>;
-
   updateDimensions = () => {
-    const containerElement = this.imagesRef.current;
+    const containerElement = this.images_ref.current;
     if (containerElement) {
         const dimensions = {
             width: containerElement.offsetWidth,
@@ -90,29 +104,29 @@ export default class Body extends Component<any, any> {
         return (
             <div className="body">
                 <section className="home">
-                    <div className='home-title' id='home' ref={this.homeRef}>
-                        <h1 style={{fontSize: '40px'}} className={this.state.homeVisible ? 'animate-after': 'homeContent-before'}>✌Greetings!✌</h1>
-                        <h2 style={h2Style} className={this.state.homeVisible ? 'animate-after': 'homeContent-before'}>My name is Robert, I am an aspiring 
+                    <div className='home-title' id='home' ref={this.home_ref}>
+                        <h1 style={{fontSize: '40px'}} className={this.state.home_visible ? 'animate-after': 'homeContent-before'}>✌Greetings!✌</h1>
+                        <h2 style={h2Style} className={this.state.home_visible ? 'animate-after': 'homeContent-before'}>My name is Robert, I am an aspiring 
                         web/software developer from Houston who is always seeking opportunites
                         to take on tough challenges and implement new technologies</h2>
-                        <h2 style={h2Style} className={this.state.homeVisible ? 'animate-after': 'homeContent-before'}>Please enjoy my personal portfolio!</h2>
+                        <h2 style={h2Style} className={this.state.home_visible ? 'animate-after': 'homeContent-before'}>Please enjoy my personal portfolio!</h2>
                     </div>
                 </section>     
                 <section className='about' id='section-about'>
-                    <div className='sub-title' id='about' ref={this.aboutRef}>
-                        <h2 className={this.state.aboutVisible ? 'animate-after': 'animate-before'}>This is the About component</h2>
+                    <div className='about-title' id='about' ref={this.about_title_ref}>
+                        <h2 className={this.state.about_title_visible ? 'animate-after': 'animate-before'}>This is the About component</h2>
                     </div>
-                    <div className="aboutMe">
+                    <div className="about-who" ref={this.about_who_ref}>
                       <div className="sub-title2">
-                        <h3 className={this.state.aboutVisible ? 'animate-after-sub': 'animate-before'}>Who am I?</h3>
+                        <h3 className={this.state.about_who_visible ? 'animate-after-sub': 'animate-before'}>Who am I?</h3>
                       </div>
-                      <p className={this.state.aboutVisible ? 'animate-after': 'animate-before'}>{Desc.AboutMe}</p>
+                      <p className={this.state.about_who_visible ? 'animate-after': 'animate-before'}>{Desc.AboutMe}</p>
                     </div>
                     <div className='hobbies' id='hobbies'>
                       <div className="sub-title2">
-                        <h3 className={this.state.aboutVisible ? 'animate-after-sub': 'animate-before'}>Hobbies</h3>
+                        <h3 className={this.state.about_visible ? 'animate-after-sub': 'animate-before'}>Hobbies</h3>
                       </div>
-                      <div className="imageSlider" ref={this.imagesRef}>
+                      <div className="imageSlider" ref={this.images_ref}>
                         <h3>Photography</h3>
                         {height && width && (
                             <SimpleImageSlider 
@@ -130,8 +144,8 @@ export default class Body extends Component<any, any> {
                     </div>
                 </section>
                 <section className="projects">
-                    <div className="sub-title" id='projects' ref={this.projectsRef}>
-                        <h2 className={this.state.projectsVisible ? 'animate-after': 'animate-before'}>This is the Projects component</h2>
+                    <div className="project-title" id='projects' ref={this.projects_ref}>
+                        <h2 className={this.state.projects_visible ? 'animate-after': 'animate-before'}>This is the Projects component</h2>
                     </div>  
 
                     <div className="project-main">
